ce25472d5f113bc9538b0260f3b60551b368ac63
Amri <amrikyaruzi@gmail.com>
2024-03-25 13:49:55 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,6 @@
-/*!
- use rand::prelude::*;
-use rayon::prelude::*;
-
-fn main() {
-    // Parameters
-    let num_trials: usize = 1000000;
-    let seq_length: usize = 1000;
-    let prob_success: f64 = 0.5;
-
-    // Calculate maximum run lengths in parallel
-    let max_run_lengths: Vec<usize> = (0..num_trials)
-        .into_par_iter()
-        .map(|_| {
-            let mut rng = thread_rng();
-            let sequence: Vec<usize> = (0..seq_length)
-                .map(|_| if rng.gen::<f64>() < prob_success { 1 } else { 0 })
-                .collect();
-
-            let max_length_in_trial = sequence
-                .iter()
-                .fold((0, 0), |(mut current_run_length, mut max_length), &sample| {
-                    if sample == 1 {
-                        current_run_length += 1;
-                        if current_run_length > max_length {
-                            max_length = current_run_length;
-                        }
-                    } else {
-                        current_run_length = 0;
-                    }
-                    (current_run_length, max_length)
-                })
-                .1;
-
-            max_length_in_trial
-        })
-        .collect();
-
-    // Calculate the mean of the maximum run lengths
-    let mean_max_length: f64 = max_run_lengths.iter().sum::<usize>() as f64 / max_run_lengths.len() as f64;
-    println!("Mean of maximum run lengths: {}", mean_max_length);
-}
-*/
-
+/*! Rust Implementation of R's RLE to estimate the maximum consecutive number of heads in 
+ a coin toss experiment*/
+ 
 use rand::prelude::*;
 use rand_distr::Binomial;
 use rayon::prelude::*;
